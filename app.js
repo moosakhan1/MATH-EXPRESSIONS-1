022f8b573bc9b101a6ad7ca00b21816336f13aff
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-12-26 22:46:51 +0500

    create

---
+++ app.js
@@ -0,0 +1,67 @@
+// let a =+prompt("enter your number 1");
+// let b =+prompt("enter your number 2");
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a%b)
+// console.log(a/b)
+// let n;
+// n=76;
+// console.log(n)
+// let c=9;
+// console.log(++c)
+// console.log(c+=7)
+// console.log(--c)
+// console.log(c%3)
+
+// let i = 600;
+// console.log(" total cost to buy ticket to a movie is " + i * 5)
+
+for (let i = 1;i<11 ; i++){
+console.log( 4 + " x " +i + " = " + i * 4)
+}
+
+// let totalMarks = 500;
+// let obtained = 400;
+// let percentage = (obtained / totalMarks)*100;
+// console.log(percentage +  " % ")
+
+// let USDollar =  104.80*10;
+// let SaudiRiyal = 28*9;
+// console.log(USDollar)
+// console.log(SaudiRiyal)
+// console.log(SaudiRiyal+USDollar)
+
+// var btn = document.getElementById("btn")
+// function display(){
+// btn.addEventListener('click',function(){
+// var text = document.getElementById("text")
+// let a= 0;
+// if(a ===0){
+//     text.innerHTML= `<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti dicta debitis quisquam animi delectus voluptas esse nobis repellendus ipsam quae, aut minus, nihil repudiandae culpa vero totam dignissimos nulla dolorem.</p>`
+
+// }
+// a++;
+// btn.removeEventListener('click',function(){
+//      if(a === 1){
+//          text.style.display = "none"
+//     }
+// })
+// })
+// }
+// display();
+
+
+// let a = 738;
+// console.log(((a+5)*10)/2)
+// let year = 2023;
+// let birthYear = 2000;
+// let age = year - birthYear
+// console.log(age)
+
+
+let r = 2;
+let π = 3.14;
+let circumference = 2*π*r;
+console.log(circumference)
+let area = π*r*r;
+console.log(area)
